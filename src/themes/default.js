b8caa39b66cc65736e56d772f97db79ad27d7c26
yashanbhore <anbhorey2003@gmail.com>
2024-04-11 17:03:07 +0530

    Initial Commit

---
+++ src/themes/default.js
@@ -0,0 +1,26 @@
+export default {
+  // Temp fonts
+  fonts: {
+    title: "Space Grotesk, sans-serif",
+    main: "Space Grotesk, sans-serif"
+  },
+  // Colors for layout - Light Theme Adjustments
+  colors: {
+    primary1: "#854CE6", // Keeping the primary color for brand consistency
+    background1: "#FFFFFF", // Light background
+    button: "#854CE6", // Keeping the button color for brand consistency
+    background2: "#F8F9FA", // Lighter shade for secondary background
+    text: "#343A40", // Darker text color for better readability
+    text1: "#212529", // Even darker shade for primary text
+    text2: "#495057", // Subdued text color for secondary text
+    text3: "#6C757D", // Greyed text color for tertiary information
+    footerBackground: "#E9ECEF" // Light footer background
+  },
+  // Breakpoints for responsive design
+  breakpoints: {
+    sm: 'screen and (max-width: 640px)',
+    md: 'screen and (max-width: 768px)',
+    lg: 'screen and (max-width: 1024px)',
+    xl: 'screen and (max-width: 1280px)'
+  },
+}
