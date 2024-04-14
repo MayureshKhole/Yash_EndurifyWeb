847f055d022932c17b11ffc7fd0c3b3ef4a3c96d
yashanbhore <anbhorey2003@gmail.com>
2024-04-14 18:04:56 +0530

    Skills Removed

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
         <Body>
           <HeroSection />
           <Wrapper>
-            <Skills />
+          
             <Experience />
           </Wrapper>
           <Projects />
